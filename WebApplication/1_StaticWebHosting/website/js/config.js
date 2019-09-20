7f1d87b86b41f8b34e02ac8789e9b2f6128a04b5
mailshilpa24 <55144526+mailshilpa24@users.noreply.github.com>
2019-09-20 11:04:14 +0200

    Update config.js

--- WebApplication/1_StaticWebHosting/website/js/config.js
+++ WebApplication/1_StaticWebHosting/website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_IYBQLlyGp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5d8689le2nofeu9nfcab5cj0t4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-st-ea2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
